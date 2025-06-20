57c4b2222e4c819a63e28b3367333c344a962b3c
MukDaniella <mugabodaniella22@gmail.com>
2025-06-20 11:50:05 -0700

    started with some edits

--- src/component/LoginPage.jsx
+++ src/component/LoginPage.jsx
@@ -7,24 +7,36 @@ const LoginPage = () => {
     <div className="login-container">
       {/* Left side: Image div */}
       <div className="image-container">
-        <img src="/images/img1.png" alt="" className="login-image" />
+         <img src={"/images/Group2.png"} alt="" className="login-image" />
       </div>
 
       {/* Right side: Login form */}
       <div className="form-container">
+        <h1 className="welcome-text">Welcome Back!</h1>
+        <p className="description-text">Sign in to access your personalized plans, discover new attractions, and continue
+          exploring the best of Rwanda. Your next adventure is just a click away!
+        </p>
+        
+        {/* Login form */}
         <form className="login-form">
           <h2>Login</h2>
           <div className="form-group">
-            <label htmlFor="username">Username</label>
-            <input 
-              type="text" 
-              id="username" 
-              name="username" 
-              placeholder="Enter your username" 
-              required 
-            />
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" name="email" placeholder="Enter your email" required />
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" name="password" placeholder="Enter your password" required />
           </div>
-          <button type="submit" className="login-button">Login</button>
+          <button type="submit" className="login-button">Login-continue exploring and planning</button>
+          <p>New to Travel? <a href="/signup">Create an account</a></p>
+          <div className="social-login">
+            <p className='forgot-password'> Remember me</p>
+          <p className="forgot-password"><a href="/forgot-password">Forgot Password?</a></p>
+          </div>
+          <div className="social-buttons">
+          <p className="google">Sign in with Google</p>
+          <p className="apple">Sign in with Apple</p>
+          <p className="facebook">Sign in with Facebook</p>
+          </div>
         </form>
       </div>
     </div>
